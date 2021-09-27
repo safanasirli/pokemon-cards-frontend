57748f6b77e51addbe44c2179770697c6b95c05a
MrGoodBurger <89281700+MrGoodBurger@users.noreply.github.com>
2021-09-27 16:54:53 -0400

    started login page layout

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+function Login() {
+    <form>
+        <div>
+            {/* <img src="" alt="Login Image"/> */}
+        </div>
+        <div>
+            <label>UserName</label>
+            <input type="text" placeholder="Enter Username"/>
+
+            <label for ="psw">Password</label>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+}
