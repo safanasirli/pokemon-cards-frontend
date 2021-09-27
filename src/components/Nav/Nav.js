8a058855df4ebc17e04f4d5dd07bbc035b3b1bb5
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-27 11:50:01 -0400

    NavBar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,14 +5,16 @@ import './Nav.css'
 function Nav() {
     return (
         <div className="nav-container">
-            <div className="logo">
+            <div className="logo-container">
                 <img className="logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c31e.png" alt="logo" />
                 <h1>Pokemon Sanctuary</h1>
             </div>
             <div className="navbar">
-                <Link to='/'>Shop</Link>
-                <Link to='/login'>Login</Link>
-                <Link to='/signup'>Signup</Link>
+                <ul>
+                    <li><Link to='/'>Shop</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/signup'>Sign up</Link></li>
+                </ul>
             </div>
         </div>
     );
