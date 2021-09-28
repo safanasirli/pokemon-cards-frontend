16f7f7e00c871f40710a46b90d7b043370f4e788
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-27 21:39:35 -0400

    Added Pokemon Information Page

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom'
 import './Pokemon.css'
 function Pokemon() {
     return (
-        <div>
-            <h1>Name</h1>
-            <h1>Description</h1>
-            <h1>Image</h1>
+        <div className="pokemon-container">
+            <section className="left">
+                <img className="pokemon-image" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" alt="Venusaur" />
+                <p>Description:A bewitching aroma wafts from its flower. The fragrance becalms those engaged in a battle. </p>
+            </section>
+            <section className="right">
+                <h1>Name: Venusaur</h1>
+                <h1>$1000</h1>
+                <button><Link to='/signup'>Reserve Pokemon</Link></button>
+            </section>
         </div>
     );
 }
