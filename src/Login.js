196d5125d193324e23f2ee115bb5648c7e21c02a
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-28 15:32:20 -0400

    Added Constant for API URL

--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,28 @@
+import React from 'react'
 function Login() {
+    return (
     <form>
-        <div class>
+        <div>
             {/* <img src="" alt="Login Image"/> */}
         </div>
-        
+
         <div>
             <label>UserName</label>
-            <input type="text" placeholder="Enter Username" name="uname" required/>
+            <input type="text" placeholder="Enter Username" name="uname" required />
 
-            <label for ="psw">Password</label>
-            <input type="password" placeholder="Enter Password" name="psw" required/>
+            <label for="psw">Password</label>
+            <input type="password" placeholder="Enter Password" name="psw" required />
 
             <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name='remember'>Remember Me</input>
-            </label>
+            <label>Remember Me </label>
+                <input type="checkbox" checked="checked" name='remember'/>
+           
         </div>
 
-        <div class="container">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <span class ="psw">Forgot <a href="#">password?</a></span>
+        <div className="container">
+            <button type="button" className="cancelbtn">Cancel</button>
+            <span className="psw">Forgot password? </span>
         </div>
     </form>
+    )
 }
