98d89285a5696e918fd51984e24cb2252e0d2945
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-27 22:36:21 -0400

    Added styles to Pokemon Info page

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Nav from '../Nav/Nav'
 import './Pokemon.css'
 function Pokemon() {
     return (
-        <div className="pokemon-container">
-            <section className="left">
-                <img className="pokemon-image" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" alt="Venusaur" />
-                <p>Description:A bewitching aroma wafts from its flower. The fragrance becalms those engaged in a battle. </p>
-            </section>
-            <section className="right">
-                <h1>Name: Venusaur</h1>
-                <h1>$1000</h1>
-                <button><Link to='/signup'>Reserve Pokemon</Link></button>
-            </section>
+        <div className="container">
+            <Nav />
+            <div className="pokemon-container">
+                <section className="left">
+                    <img className="pokemon-image" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png" alt="Venusaur" />
+
+                </section>
+                <section className="right">
+                    <h1>Venusaur</h1>
+                    <h2>$1000</h2>
+                    <p className="description">A bewitching aroma wafts from its flower. The fragrance becalms those engaged in a battle. </p>
+                    <button className="reserve-button"><Link to='/signup'>Reserve Pokemon</Link></button>
+                </section>
+            </div>
         </div>
     );
 }
