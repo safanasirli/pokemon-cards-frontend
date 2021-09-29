14a58ebe471b01f4cbe27c1e32bc1eecd9485885
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-29 09:01:00 -0400

    Styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,14 +5,14 @@ function Footer() {
     return (
         <div className="footer-row">
             <div className="col">
-                <h3>Company</h3>
+                <h2>Company</h2>
                 <ul className="footer-list">
                     <li>About Us</li>
                     <li>Contact Us</li>
                 </ul>
             </div>
             <div className="col">
-                <h3>FAQ</h3>
+                <h2>FAQ</h2>
                 <ul className="footer-list">
                     <li>Payment Policy</li>
                     <li>Shipping Terms</li>
