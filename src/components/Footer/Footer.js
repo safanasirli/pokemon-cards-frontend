2c40f322647c70d0bf8b7d3a5e11ad7846c5dd82
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-29 08:50:29 -0400

    Added Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,22 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div>
-           <ul>
+        <div className="footer-row">
+            <div className="col">
+                <h3>Company</h3>
+                <ul className="footer-list">
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div className="col">
+                <h3>FAQ</h3>
+                <ul className="footer-list">
+                    <li>Payment Policy</li>
+                    <li>Shipping Terms</li>
+                </ul>
+            </div>
 
-           </ul>
         </div>
     );
 }
