1b2fc7d989dabd4077b9cb94218d70bbcb1ca32c
Annie Lee <annie80113@hotmail.com>
2021-09-28 21:46:49 -0400

    organized code for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,15 +2,15 @@
 function Login() {
     return (
         <form>
-            <div class>
-                {/* <img src="" alt="Login Image"/> */}
-            </div>
+            {/* <div>
+                {/* <img src="" alt="Login Image"/>
+            </div> */}
 
-            <div>
+            <div className='login-container'>
                 <label>UserName</label>
                 <input type="text" placeholder="Enter Username" name="uname" required />
 
-                <label for="psw">Password</label>
+                <label htmlFor="psw">Password</label>
                 <input type="password" placeholder="Enter Password" name="psw" required />
 
                 <button type="submit">Login</button>
@@ -19,9 +19,9 @@ function Login() {
                 </label>
             </div>
 
-            <div class="container">
-                <button type="button" class="cancelbtn">Cancel</button>
-                <span class="psw">Forgot <a href="/">password?</a></span>
+            <div className="container">
+                <button type="button" className="cancelbtn">Cancel</button>
+                <span className="psw">Forgot <a href="/">password?</a></span>
             </div>
         </form>
     )
