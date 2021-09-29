bc3faa75dfa9dc65939acf4d2e86a860388254fb
MrGoodBurger <89281700+MrGoodBurger@users.noreply.github.com>
2021-09-28 22:13:04 -0400

    Need to fix button on page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,29 +2,37 @@ import React from 'react';
 import './Login.css';
 import avatarPic from '../../images/avatar.png';
 
+
 function Login() {
     return(
     <form>
         <div className="imgcontainer">
-            <img src={avatarPic} alt="avatar silouette" class="avatar"/>
+            <img src={avatarPic} alt="avatar silouette" className="avatar"/>
         </div>
-        
+        {/* Give DIV a class, have DIV  */}
+        {/* Use flexbox */}
+        <div> 
+            <div>
+                <label>Username</label>
+                <input type="text" placeholder="Enter Username" name="uname" required/>
+
+                <label for ="psw">Password</label>
+                <input type="password" placeholder="Enter Password" name="psw" required/>
+        </div>
         <div>
-            <label>UserName</label>
-            <input type="text" placeholder="Enter Username" name="uname" required/>
-
-            <label for ="psw">Password</label>
-            <input type="password" placeholder="Enter Password" name="psw" required/>
-
-            <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name='remember'/>Remember Me
-            </label>
+                <button type="submit">Login</button>
+                <button type="button" className="cancelbtn">Cancel</button>
+        </div>
+            <br/>
+            
         </div>
 
         <div class="container">
-            <button type="button" className="cancelbtn">Cancel</button>
-            <span class ="psw">Forgot <a href="#">password?</a></span>
+            
+                <label>
+                    <input type="checkbox" checked="checked" name='remember'/>Remember Me
+                </label>
+            <span className="psw">Forgot <a href="#">password?</a></span>
         </div>
     </form>
     )
