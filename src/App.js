e9e15c170b5d83935ebe406c7236ace7fdd86869
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-30 10:41:03 -0400

    Removed Login/Signup Page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
 import Pokemon from './components/Pokemon/Pokemon';
 import Checkout from './components/Checkout/Checkout';
 import Cart from './components/Checkout/Cart';
 
 function App() {
-  const[logged, setLogged]= useState(false);
+  const [logged, setLogged] = useState(false);
   return (
     <div className="App">
       {
-        logged ? 
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/pokemons/:id' component={Pokemon} />
-          <Route exact path='/cart' component={Cart} />
-          <Route exact path='/checkout' component={Checkout} />
-        </Switch>
-            :
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' render={props => <Login setLogged={setLogged} logged ={logged}
-          {...props} />} />
-          <Route exact path='/signup' component={Signup} />
-          <Route path='/pokemons/:id' component={Pokemon} />
-          <Route exact path='/cart' component={Cart} />
-          <Route exact path='/checkout' component={Checkout} />
-        </Switch>
+        logged ?
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/pokemons/:id' component={Pokemon} />
+            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/checkout' component={Checkout} />
+          </Switch>
+          :
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/pokemons/:id' component={Pokemon} />
+            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/checkout' component={Checkout} />
+          </Switch>
       }
     </div>
   );
