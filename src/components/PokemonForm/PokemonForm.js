c3e6eb1215852168075f14eca959651e18cfcec9
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-30 18:20:17 -0400

    Update in the PokemonForm

--- src/components/PokemonForm/PokemonForm.js
+++ src/components/PokemonForm/PokemonForm.js
@@ -2,52 +2,55 @@ import React from 'react';
 import './PokemonForm.css';
 
 const PokemonForm = ({ pokemon, handleSubmit, handleChange }) => (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="name">Name</label>
-      <input 
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="name">Name
+      <input
         placeholder="Pokemon Name"
-        value={pokemon.name }
+        value={pokemon.name}
         name="name"
-        onChange={ handleChange }
-        required 
+        onChange={handleChange}
+        required
         id="name"
       />
     </label>
     <label htmlFor="image">Image
-      <input 
+      <input
         placeholder="Pokemon Image"
-        value={pokemon.image }
+        value={pokemon.image}
         name="image"
-        onChange={ handleChange }
+        onChange={handleChange}
         id="image"
       />
     </label>
-      <label htmlFor="description">Description</label>
-      <input 
-          placeholder="Pokemon Description"
-          value={ pokemon.description }
-          name="description"
-          onChange={ handleChange } 
-          id="description"
-        />
-      <label htmlFor="price">Price</label>
-      <input 
-          placeholder="Pokemon Price"
-          value={ pokemon.price }
-          name="price"
-          onChange={ handleChange } 
-          id="price"
-        />
-      <label htmlFor="image">Image</label>
-      <input 
-          placeholder="Pokemon Image URL"
-          value={ pokemon.image }
-          name="image"
-          onChange={ handleChange }
-          id="image"
-        />
-      <button type="submit">Submit</button>
-    </form>
+    <label htmlFor="description">Description
+      <input
+        placeholder="Pokemon Description"
+        value={pokemon.description}
+        name="description"
+        onChange={handleChange}
+        id="description"
+      />
+    </label>
+    <label htmlFor="price">Price
+      <input
+        placeholder="Pokemon Price"
+        value={pokemon.price}
+        name="price"
+        onChange={handleChange}
+        id="p"
+      />
+    </label>
+    <label htmlFor="image">Image
+      <input
+        placeholder="Pokemon Image URL"
+        value={pokemon.image}
+        name="image"
+        onChange={handleChange}
+        id="image"
+      />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
 );
 
 export default PokemonForm;
