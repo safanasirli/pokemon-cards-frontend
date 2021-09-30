539785f2370d8b585694220894a05cbe28dc1880
Annie Lee <annie80113@hotmail.com>
2021-09-29 21:54:05 -0400

    uncommented checkout route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import Pokemon from './components/Pokemon/Pokemon';
-// import Checkout from './components/Checkout/Checkout';
+import Checkout from './components/Checkout/Checkout';
 
 function App() {
   return (
